ce63fa66d859ac1057a2cd601b8fb1e0c211362f
Andy Coupe <andrewmcoupe@gmail.com>
2021-07-30 15:53:59 +0100

    Init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+console.log(`
+👋 Andy Coupe - Software Engineer 👋
+
+Thanks for npx'ing me and welcoming me into your terminal.
+
+🐦 Twitter: https://twitter.com/andycoupedev
+📖 GitHub: https://github.com/andrewmcoupe
+📝 Blog: https://dev.to/andrewmcoupe
+`);
