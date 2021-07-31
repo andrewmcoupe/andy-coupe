c77f4e7517d3c9e418e0e6baac2453c462c4ed89
Andy Coupe <andrewmcoupe@gmail.com>
2021-07-31 06:50:49 +0100

    Add cli-welcome

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 #!/usr/bin/env node
-const clearConsole = require("clear-any-console");
+const welcome = require("cli-welcome");
+const packageJsonFile = require("./package.json");
 
-// Clears the console for better readability
-clearConsole();
+welcome({
+  title: packageJsonFile.name,
+  tagLine: "Hola!",
+  description: packageJsonFile.description,
+  version: packageJsonFile.version,
+  bgColor: "#FADC00",
+  bold: true,
+  clear: true,
+});
 
 console.log(`
 👋 Andy Coupe - Software Engineer 👋
 
 Thanks for npx'ing me and welcoming me into your terminal.
+
+  ===========
+
+  I'm Andy Coupe, a software engineer based in Manchester, UK. 
+  I'm currently working at Yozobi building cool things with cool technologies 
+  such as TypeScript, React, NodeJS, GraphQL, AWS and more!
+
+  I love ⚽️ and I'm a big fan of Liverpool FC. I'm also the proud owner 
+  of a Siberian Husky named Ice! 🧊
+
+  Find out more about me using 
+  my social links below 👇
+
+  ===========
 
 🐦 Twitter: https://twitter.com/andycoupedev
 📖 GitHub: https://github.com/andrewmcoupe
